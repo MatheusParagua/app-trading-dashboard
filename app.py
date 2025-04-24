5ff9faf59b53917c821c55441840614f6412f1f0
MatheusParagua <paraguassu_santos@hotmail.com>
2025-04-24 18:41:08 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-
 import streamlit as st
+from pages.dashboard import show_dashboard
 
-st.set_page_config(page_title="Painel de Trading", layout="wide")
-
+st.set_page_config(page_title="Painel de Trading Esportivo", layout="wide")
 st.title("Painel de Trading Esportivo")
+st.markdown("Aqui você verá todos os gráficos e análises do seu desempenho.")
 
-st.write("Aqui você verá todos os gráficos e análises do seu desempenho.")
-
-st.success("Dashboard carregado com sucesso.")
+# Chamada do dashboard principal
+show_dashboard()
